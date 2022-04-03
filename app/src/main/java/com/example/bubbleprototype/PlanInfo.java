f94afd3deb8bb9e0eac9d9b4eb0e2afbce8beb6b
Dan <drblack651@gmail.com>
2022-04-03 17:10:16 -0500

    plan info

--- app/src/main/java/com/example/bubbleprototype/PlanInfo.java
+++ app/src/main/java/com/example/bubbleprototype/PlanInfo.java
@@ -12,23 +12,5 @@ public class PlanInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_info);
-
-//        // calling the action bar
-//        ActionBar actionBar = getSupportActionBar();
-//
-//        // showing the back button in action bar
-//        actionBar.setDisplayHomeAsUpEnabled(true);
     }
-
-    // this event will enable the back
-    // function to the button on press
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                this.finish();
-//                return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
 }
